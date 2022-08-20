05d6dac05ee048acf570309a8d8d1ce8fd8c8442
rasoul <rasoulansarinavan98@gmail.com>
2022-08-20 20:41:42 +0430

    update_web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Client\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -23,13 +24,13 @@ Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware(['auth:web','PreventBackHistory'])->group(function () {
+Route::middleware(['auth:web', 'PreventBackHistory'])->group(function () {
     Route::get('home', function () {
         return 'home view';
     });
 });
 
-Route::middleware(['guest:web','PreventBackHistory'])->group(function () {
+Route::middleware(['guest:web', 'PreventBackHistory'])->group(function () {
 
     Route::get('/', function () {
         return 'home view';
@@ -39,4 +40,20 @@ Route::middleware(['guest:web','PreventBackHistory'])->group(function () {
     Route::get('/login/store', [UserController::class, 'LoginStore'])->name('user.check');
     Route::get('/register', [UserController::class, 'RegisterForm'])->name('user.register');
     Route::post('/register/store', [UserController::class, 'RegisterStore'])->name('user.store');
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::middleware(['auth:admin', 'PreventBackHistory'])->group(function () {
+        Route::get('/home', function () {
+            return view('admin.home');
+        })->name('home');
+    });
+
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function () {
+        Route::get('/login', function () {
+            return view('admin.auth.login');
+        })->name('login');
+        Route::post('/login/store', [AdminController::class, 'LoginStore'])->name('check');
+    });
 });
