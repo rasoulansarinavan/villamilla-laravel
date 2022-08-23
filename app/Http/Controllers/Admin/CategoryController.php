b8a501a7fbb771eb4f82e7e333b3cd827e4e463d
rasoul <rasoulansarinavan98@gmail.com>
2022-08-23 22:06:55 +0430

    add_CategoryController

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $categories = Category::query()->latest()->get();
+        return view('admin.categories.index', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(CategoryStoreRequest $request)
+    {
+        $name_fa = $request->get('name_fa');
+        $name_en = $request->get('name_en');
+        $category_id = $request->get('category_id');
+
+        Category::query()->create([
+            'name_fa' => $name_fa,
+            'name_en' => $name_en,
+            'slug_en' => strtolower(str_replace(' ', '_', $name_en)),
+            'slug_fa' => str_replace(' ', '_', $name_fa),
+            'category_id' => $category_id,
+        ]);
+        return redirect()->back()->with('success', 'دسته بندی با موفقیت افزوده شد');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
