ba0a5f3006883eeddbb75d3a9fdc302386e4e12f
rasoul <rasoulansarinavan98@gmail.com>
2022-08-23 08:45:20 +0430

    add_index.blade_admin_environments

---
+++ resources/views/admin/environments/index.blade.php
@@ -0,0 +1,136 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+            <div class="content-body">
+                <!-- Zero configuration table -->
+                <section id="basic-datatable">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">لیست محیط های گردشگری</h4>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body card-dashboard">
+                                        <div class="table-responsive">
+                                            <table class="table zero-configuration">
+                                                <thead>
+                                                <tr>
+                                                    <th>ردیف</th>
+                                                    <th>نام محیط به فارسی</th>
+                                                    <th>نام محیط به انگلیسی</th>
+                                                    <th>تصویر</th>
+                                                    <th>عملیات</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                @foreach($environments as $environment)
+                                                    <tr>
+                                                        <td></td>
+                                                        <td>{{$environment->name_fa}}</td>
+                                                        <td>{{$environment->name_en}}</td>
+                                                        <td>
+                                                            <img src="/images/environments/{{$environment->image}}"
+                                                                 width="70" alt="">
+                                                        </td>
+                                                        <td>
+                                                            <a class="badge badge-primary"
+                                                               href="{{route('admin.environments.edit',$environment->id)}}">ویرایش</a>
+                                                            <a class="badge badge-danger" href="">حذف</a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                                </tbody>
+                                                </tfoot>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">افزودن محیط گردشگری</h4>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <form class="form form-vertical" action="{{route('admin.environments.store')}}"
+                                              method="post" enctype="multipart/form-data">
+                                            @csrf
+                                            <div class="form-body">
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label for="first-name-vertical">نام محیط گردشگری به
+                                                                فارسی</label>
+                                                            <input type="text" id="first-name-vertical"
+                                                                   class="form-control" name="name_fa"
+                                                                   placeholder="">
+                                                            <span
+                                                                class="text-danger">@error('name_fa'){{$message}}@enderror</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label for="first-name-vertical">نام محیط گردشگری به
+                                                                انگلیسی</label>
+                                                            <input type="text" id="first-name-vertical"
+                                                                   class="form-control" name="name_en"
+                                                                   placeholder="">
+                                                            <span
+                                                                class="text-danger">@error('name_en'){{$message}}@enderror</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label>تصویر محیط گردشگری</label>
+                                                            <div class="custom-file">
+                                                                <input name="image" type="file"
+                                                                       class="custom-file-input"
+                                                                       id="inputGroupFile04">
+                                                                <label class="custom-file-label"
+                                                                       for="inputGroupFile04">انتخاب تصویر
+                                                                    محیط گردشگری</label>
+                                                                <span
+                                                                    class="text-danger">@error('image'){{$message}}@enderror</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-12 d-flex justify-content-end">
+                                                        <button type="submit" class="btn btn-primary mr-1 mb-1">
+                                                            ثبت
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--/ Zero configuration table -->
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+@endsection
+
+@section('scripts')
+
+    <script src="/admin/app-assets/vendors/js/tables/datatable/datatables.min.js"></script>
+    <script src="/admin/app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js"></script>
+    <script src="/admin/app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js"></script>
+
+    <!-- BEGIN: Page JS-->
+    <script src="/admin/app-assets/js/scripts/datatables/datatable.js"></script>
+    <!-- END: Page JS-->
+
+@endsection
