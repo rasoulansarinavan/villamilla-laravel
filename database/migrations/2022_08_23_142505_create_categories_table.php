ba8432cb9da1d5ce4035338e3a6629cfa1b6a454
rasoul <rasoulansarinavan98@gmail.com>
2022-08-23 22:07:46 +0430

    add_categories_table

---
+++ database/migrations/2022_08_23_142505_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_fa')->unique();
+            $table->string('name_en')->unique();
+            $table->string('slug_fa')->unique();
+            $table->string('slug_en')->unique();
+            $table->foreignId('category_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
