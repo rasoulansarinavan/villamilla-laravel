cac39eb6c323b7b90940d55ca0aff5e8be6ddfc7
rasoul <rasoulansarinavan98@gmail.com>
2022-08-26 18:21:04 +0430

    add_categoryStoreRequest

---
+++ app/Http/Requests/CategoryStoreRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name_fa' => 'required|unique:categories',
+            'name_en' => 'required|unique:categories',
+            'category_id' => 'integer|exists:categories,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name_fa.unique' => 'نوع اقامتگاه تکراری است',
+            'name_fa.required' => 'نوع اقامتگاه الزامی است',
+            'name_en.unique' => 'نوع اقامتگاه تکراری است',
+            'name_en.required' => 'نوع اقامتگاه الزامی است',
+            'category_id.exists'=>'دسته والد وجود ندارد',
+        ];
+    }
+}
