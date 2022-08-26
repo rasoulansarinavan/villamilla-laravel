332b84988ef2c5f08d77bf5c696fa3eb8b3faacb
rasoul <rasoulansarinavan98@gmail.com>
2022-08-26 18:22:08 +0430

    update_environment.php

--- app/Models/Environment.php
+++ app/Models/Environment.php
@@ -2,45 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class Environment extends Authenticatable
+class Environment extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name_fa',
-        'slug_fa',
-        'name_en',
-        'slug_en',
-        'image',
-    ];
+    protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function parent()
+    {
+        return $this->belongsTo(Category::class,'category_id');
+    }
 }
