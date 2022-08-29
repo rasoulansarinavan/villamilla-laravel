2c93e5d00b5109b2eac912a3289b6c69d228b64f
rasoul <rasoulansarinavan98@gmail.com>
2022-08-29 19:45:38 +0430

    add_residences_table

---
+++ database/migrations/2022_08_28_135606_create_residences_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('residences', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_fa')->unique();
+            $table->string('name_en')->unique();
+            $table->string('slug_fa');
+            $table->string('slug_en');
+            $table->string('wc_fa');
+            $table->string('wc_en');
+            $table->string('bath');
+            $table->string('single_bed');
+            $table->string('double_bed');
+            $table->integer('capacity');
+            $table->integer('price');
+            $table->integer('discount')->nullable();
+            $table->text('long_desc_fa');
+            $table->text('long_desc_en');
+            $table->string('thumbnail');
+            $table->integer('special_offer')->nullable();
+            $table->integer('status')->default(0);
+            $table->foreignId('environment_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('residences');
+    }
+};
