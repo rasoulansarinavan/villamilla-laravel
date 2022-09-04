8dcac84cdefb8d627ae8f287923f1cc7b50154f1
rasoul <rasoulansarinavan98@gmail.com>
2022-09-04 09:01:25 +0430

    update_cascadeDelete_galleries_table

--- database/migrations/2022_08_29_164044_create_galleries_table.php
+++ database/migrations/2022_08_29_164044_create_galleries_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('residence_id')->constrained();
+            $table->foreignId('residence_id')->constrained()->cascadeOnDelete();
             $table->string('image');
             $table->timestamps();
         });
